dc8008748207b56d53bcf921c5774e723625388f
MrChileball <89463623+MrChileball@users.noreply.github.com>
2023-07-14 03:57:41 +0000

    SERVICE WORKER.JS

--- javascript/service-worker.js
+++ javascript/service-worker.js
@@ -1,11 +1,38 @@
+// Registrando el Service Worker
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
         console.log('Service Worker registrado con éxito:', registration);
       })
-      .catch(function(error) {
-        console.log('Fallo en el registro del Service Worker:', error);
+      .catch(error => {
+        console.error('Error al registrar el Service Worker:', error);
       });
   });
 }
+
+// Evento install del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/css/styles.css',
+        '/javascript/module.js',
+        '/images/favicon.png',
+        '/images/',
+      ]);
+    })
+  );
+});
+
+// Evento fetch del Service Worker
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
