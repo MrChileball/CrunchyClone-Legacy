12f19fb20857b9dc82554dff3787007312159ec5
MrChileball <89463623+MrChileball@users.noreply.github.com>
2023-07-14 06:02:05 +0000

    dasdsa

---
+++ episode-manager.js
@@ -0,0 +1,34 @@
+// Función para redireccionar a un episodio específico
+function redirectToEpisode(episode) {
+    window.location.href = `https://shingekionline.netlify.app/season1?=episode${episode}`;
+  }
+  
+  // Función para cargar el episodio correspondiente al valor del query
+  function loadEpisodeFromQuery() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const episode = urlParams.get('episode');
+  
+    if (episode) {
+      fetch('episode-list.json')
+        .then(response => response.json())
+        .then(data => {
+          if (data.hasOwnProperty(episode)) {
+            const episodeUrl = data[episode];
+            const videoPlayer = document.getElementById('videoPlayer');
+            videoPlayer.src = episodeUrl;
+            videoPlayer.load();
+          } else {
+            console.error(`No se encontró información para el episodio "${episode}".`);
+          }
+        })
+        .catch(error => {
+          console.error('Error al cargar el archivo episode-list.json:', error);
+        });
+    } else {
+      console.error('No se proporcionó ningún parámetro de episodio en la URL.');
+    }
+  }
+  
+  // Cargar el episodio correspondiente cuando se cargue la página
+  loadEpisodeFromQuery();
