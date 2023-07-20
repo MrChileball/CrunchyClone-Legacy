41717967218114db41963089310db2b3a86d323d
MrChileball <89463623+MrChileball@users.noreply.github.com>
2023-07-20 03:18:14 +0000

    test module.js

--- javascript/module.js
+++ javascript/module.js
@@ -4,30 +4,35 @@
 * @param {string} contenedorId - El ID del contenedor en el que se cargará el contenido.
 */
 function cargarContenido(url, contenedorId) {
-    fetch(url)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById(contenedorId).innerHTML = data;
-      })
-      .catch(error => {
-        console.error(`Error al cargar el contenido desde ${url}: ${error}`);
-      });
-   }
-  
-   
-   // Carga los elementos desde un archivo externo (elementos.json)
-   fetch("/web-modules/modules.json")
-    .then(response => response.json())
+  const contenedor = document.getElementById(contenedorId);
+  if (!contenedor) {
+    console.error(`El contenedor con el ID '${contenedorId}' no se encontró en la página.`);
+    return;
+  }
+
+  fetch(url)
+    .then(response => response.text())
     .then(data => {
-      // Carga el contenido para cada elemento en paralelo
-      Promise.all(
-        data.map(elemento =>
-          cargarContenido(elemento.url, elemento.contenedorId)
-        )
-      ).then(() => {
-        console.log("Todos los elementos se han cargado correctamente.");
-      });
+      contenedor.innerHTML = data;
     })
     .catch(error => {
-      console.error(`Error al cargar los elementos: ${error}`);
-    }); 
+      console.error(`Error al cargar el contenido desde ${url}: ${error}`);
+    });
+}
+  
+// Carga los elementos desde un archivo externo (elementos.json)
+fetch("/web-modules/modules.json")
+  .then(response => response.json())
+  .then(data => {
+    // Carga el contenido para cada elemento en paralelo
+    Promise.all(
+      data.map(elemento =>
+        cargarContenido(elemento.url, elemento.contenedorId)
+      )
+    ).then(() => {
+      console.log("Todos los elementos se han cargado correctamente.");
+    });
+  })
+  .catch(error => {
+    console.error(`Error al cargar los elementos: ${error}`);
+  });
