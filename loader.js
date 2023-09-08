60db69efbcae7c61d1ccaf8de73c0cf9a72d582c
MrChileball <chileball5@gmail.com>
2023-09-08 15:16:59 -0300

    EpisodeListGen.js | Dynamic episode list for series

--- loader.js
+++ loader.js
@@ -1,9 +1,9 @@
 // mainDefer.js
 // SOLO SCRIPTS PEQUEÑOS Y ESENCIALES+
-
+import '/javascript/module.js';
 import '/javascript/clipboard.js';
 import '/javascript/loadingDOM.js';
-import '/javascript/module.js';
-import '/service-worker.js';
+
+//import '/service-worker.js';
 // Importa otros scripts aquí si es necesario
 
