65e00c958fd621995e2b4ad38eda6c6a6a1c5159
MrChileball <chileball5@gmail.com>
2023-09-10 17:40:57 -0300

    Season selector | EpisodelistGen.js

--- javascript/episodeListGen.js
+++ javascript/episodeListGen.js
@@ -1,23 +1,36 @@
-
+let href = window.location.href.slice(0, window.location.href.length - 10);;
+let playerHref = href + "player.html";
+console.log(href);
+console.log(playerHref);
+let episodeCounter = 0;
 // función asíncrona para leer datos de episodios ubicados en 
-async function getEpisodes() {
+async function getEpisodes(season) {
     let listFile = 'episodes.json';
 
     try {
         let res = await fetch(listFile);
-        return await res.json();
+        let data = await res.json();
+        
+        // Asegúrate de que season sea uno de los valores válidos (s1, s2, s3, s4, music)
+        if (data.hasOwnProperty(season)) {
+            return data[season];
+        } else {
+            console.error(`No se encontró la temporada "${season}" en el archivo JSON.`);
+            return [];
+        }
     } catch (error) {
         console.log(error);
     }
 }
 
 
-async function renderEpisodes(){
+async function renderEpisodes(season){
 
-    let users = await getEpisodes();
+    let users = await getEpisodes(season);
     let htmlContent = '';
     console.log(users);
     users.forEach(episode => {
+    
         let htmlSegment = `
         <div class="chapter-item">
                 <picture>
@@ -28,7 +41,7 @@ async function renderEpisodes(){
                     <p>Attack on titan - S1</p>
                     <div class="chapter-text">
                         <h4 class="custom-padding">${episode.title}</h4>
-                        <button  onclick="window.location.href = '/series/snk/seasons/1/shingeki-season1.html?ep=1'" class="flex button">
+                        <button  onclick="window.location.href = '${playerHref}?ep=${episodeCounter}'" class="flex button">
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon-tabler icon-tabler-player-play" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                 <path d="M7 4v16l13 -8z"></path>
@@ -45,6 +58,8 @@ async function renderEpisodes(){
 
         console.log("Contenido añadido a la variable HtmlContent");
         htmlContent += htmlSegment;
+        episodeCounter = episodeCounter + 1;
+        
         
     });
 
@@ -54,7 +69,27 @@ async function renderEpisodes(){
     console.log("innerhtml listo")
 
 }
+// Script que añade listeners a cada botón selector de temporada o categoría
+const links = document.querySelectorAll('.dropdown-content a');
+
+        // Busca un elemento con clase chapter-list
+        const chapterList = document.querySelector('.chapter-list');
+
+        //por cada elemento de tipo a hijo de un elemento clase dropdown-content, crea un bucle
+        links.forEach(link => {
+            //añade un listener de click, cada vez que clickea
+            link.addEventListener('click', function(event) {
+                event.preventDefault();
+                //busca el atributo data-season que contiene el elemento HTML
+                const season = this.getAttribute('data-season');
+                //renderiza una categoría nueva en base al atributo
+                renderEpisodes(season);
+            });
+        });
+
+// Espera cerca de un segundo para ejecutar el renderEpisodes con el arg. s1 (Primera temporada)
 setTimeout(() => {
-    renderEpisodes();
+    renderEpisodes("s1");
   }, "1000");
+
 
