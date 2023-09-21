cfd3f7ec17c32ff58e92e91f95671fb2f8cc981a
MrChileball <chileball5@gmail.com>
2023-09-20 22:22:01 -0300

    Dynamic background for seasons

--- javascript/mediaContent.js
+++ javascript/mediaContent.js
@@ -1,3 +1,22 @@
-const backgroundContainer = document.getElementById('key-art');
+// = document.getElementsByClassName('main-background');
 
-backgroundContainer.style.backgroundImage
+//var element = document.getElementsByClassName("main-background");
+//element.style.background = "url('https://cdn.xgqfrms.xyz/logo/logo.png')";
+
+// Obtener el elemento con la clase "main-background"
+
+function test() {
+    // Obtener el elemento con la clase "main-background"
+    const mainBackground = document.querySelector('.main-background');
+
+    if (mainBackground) {
+        // Cambiar la propiedad de fondo
+        mainBackground.style.background = 'url(/series/snk/content/banner-mobile.webp)';
+    } else {
+        //ELEMENTO NO CARGADO, CONEXIÓN MUY LENTA O UN ERROR
+    }
+};
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener el elemento con la clase "main-background"
+    setTimeout(test, 450);
+});
